68ca7ca8ed01ccd4e61f94c1c97bf444ce376a97
Antonio Valentino <antonio.valentino@tiscali.it>
2017-11-26 20:38:21 +0100

    Path specification is now mandatory for check and fix sub-commands

--- fmtcheck.py
+++ fmtcheck.py
@@ -589,7 +589,7 @@ def get_check_parser(parser=None):
         '-c', '--config', help='path to the configuration file')
 
     parser.add_argument(
-        'paths', default='.', nargs='*', metavar='PATH',
+        'paths', nargs='+', metavar='PATH',
         help='root of the source tree to scan (default: %(default)r)')
 
     return parser
@@ -636,7 +636,7 @@ def get_fix_parser(parser=None):
         '-c', '--config', help='path to the configuration file')
 
     parser.add_argument(
-        'paths', default='.', nargs='*', metavar='PATH',
+        'paths', nargs='+', metavar='PATH',
         help='root of the source tree to scan (default: %(default)r)')
 
     return parser
